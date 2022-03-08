9e2e5648ee66eeb59a798aa0aa4980861ea6d5a5
강동윤 <davidkang8111@gmail.com>
2022-03-08 17:35:33 +0900

    20220308

--- UniRun/Assets/02.Scripts/PlayerController.cs
+++ UniRun/Assets/02.Scripts/PlayerController.cs
@@ -87,7 +87,8 @@ public class PlayerController : MonoBehaviour
         // 사망 효과음 재생
         playerAudio.Play();
 
-        // 속도를 제로(0,0)로 변경
+        // 속도를 제로(0,0
+        // )로 변경
         playerRigidbody.velocity = Vector2.zero;
         // 나 사망했어~ 사망 상태를 true로 변경
         isDead = true;
