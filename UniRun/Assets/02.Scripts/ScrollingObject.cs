640d613aac80e7ed8fad6de9338315bbbe42b659
강동윤 <davidkang8111@gmail.com>
2022-03-10 17:26:11 +0900

    20220310

--- UniRun/Assets/02.Scripts/ScrollingObject.cs
+++ UniRun/Assets/02.Scripts/ScrollingObject.cs
@@ -9,7 +9,10 @@ public class ScrollingObject : MonoBehaviour
 
     void Update()
     {
-        // 초당 speed의 속도로 왼쪽으로 평행이동 구현
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동 구현
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
