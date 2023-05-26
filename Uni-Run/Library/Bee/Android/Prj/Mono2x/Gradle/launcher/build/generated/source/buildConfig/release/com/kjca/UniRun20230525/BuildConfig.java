cf7cee21272af6596e8419cd78c638883d6706c1
강동윤 <davidkang8111@gmail.com>
2023-05-26 12:47:53 +0900

    20230526
    20230526

---
+++ Uni-Run/Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/kjca/UniRun20230525/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kjca.UniRun20230525;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kjca.UniRun20230525";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
